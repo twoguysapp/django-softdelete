ed62612fcf8b824fc6cef2037735861619e87b1b
Stefan Haflidason <styrmis@gmail.com>
2014-04-19 13:26:50 +0100

    Don't show soft-deleted objects in admin by default

--- softdelete/admin/admin.py
+++ softdelete/admin/admin.py
@@ -24,27 +24,14 @@ class SoftDeleteObjectInline(admin.TabularInline):
         return qs
 
 class SoftDeleteObjectAdmin(admin.ModelAdmin):
-    form = SoftDeleteObjectAdminForm
-    actions = ['delete_selected', 'soft_undelete']
-
-    def delete_selected(self, request, queryset):
-        queryset.delete()
-    delete_selected.short_description = 'Soft delete selected objects'
-
-    def soft_undelete(self, request, queryset):
-        queryset.undelete()
-    soft_undelete.short_description = 'Undelete selected objects'
-
-    def response_change(self, request, obj, *args, **kwargs):
-        if request.POST.has_key('undelete'):
-            return HttpResponseRedirect('../')
-        return super(SoftDeleteObjectAdmin, self).response_change(request, obj, *args, **kwargs)
+    exclude = ( 'deleted_at', )
 
     def queryset(self, request):
-        try:
-            qs = self.model._default_manager.all_with_deleted()
-        except Exception as ex:
-            qs = self.model._default_manager.all()
+        # Do not show deleted records by default.
+        # The way to undelete records will either be
+        # via a changeset or the individual soft-deletion
+        # records.
+        qs = self.model._default_manager.all()
 
         ordering = self.ordering or ()
         if ordering:
