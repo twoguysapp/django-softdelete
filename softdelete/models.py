95b68eed5f7ea46098bdcb9d9f5965e6b751f1d8
Kevin Wong <wong.wlk@gmail.com>
2015-08-24 17:05:40 -0500

    commented out generic

--- softdelete/models.py
+++ softdelete/models.py
@@ -6,7 +6,7 @@ from django.db.models import query
 from django.db import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+# from django.contrib.contenttypes import generic
 from django.contrib.auth.models import Group, Permission
 from django.utils import timezone
 from django.contrib.contenttypes.fields import GenericRelation, GenericForeignKey
